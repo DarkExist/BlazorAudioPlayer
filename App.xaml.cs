83a3ddff2ab71772262c7d01ddf41ee51fdf8167
relog <relogotech@gmail.com>
2024-06-09 22:42:11 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorAudioPlayer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            const int newWidth = 700;
+            const int newHeight = 700;
+            window.Width = newWidth;
+            window.Height = newHeight;
+            return window;
+        }
+    }
+}
